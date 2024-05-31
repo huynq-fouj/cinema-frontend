be5273aa06885e4eb94b03d0ebcfd5e7d265cfe4
Nguyễn Quang Huy <nguyenquanghuylt2002@gmail.com>
2024-05-31 19:00:08 +0700

    Update

--- src/pages/admin/layouts/AdminLayout.tsx
+++ src/pages/admin/layouts/AdminLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/header/Header";
 import Sidebar from "../components/sidebar/Sidebar";
